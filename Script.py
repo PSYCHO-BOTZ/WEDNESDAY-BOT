453db14a5324a2872d4883b2f66e57e992775f9a
Psycho Botz <124176508+PSYCHO-BOTZ@users.noreply.github.com>
2023-02-25 12:09:43 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -2,19 +2,20 @@ class script(object):
     HELP_TXT = """𝙷𝙴𝚈 {}\n𝙷𝙴𝚁𝙴 𝙸𝚂 𝙼𝚈 𝙷𝙴𝙻𝙿 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
 
     ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴 : {}
-✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁 : <a href=https://t.me/mr_MKN>𝙈𝙧.𝙈𝙆𝙉 𝙏𝙂</a>
-✯ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈 : 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
-✯ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴 : 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
-✯ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴 : 𝙼𝙾𝙽𝙶𝙾-𝙳𝙱
-✯ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁 : 𝙰𝙽𝚈𝚆𝙷𝙴𝚁𝙴
-✯ 𝙱𝚄𝙸𝙻𝙳 𝚅𝙴𝚁𝚂𝙸𝙾𝙽: 𝙿𝚁𝙾𝙵𝙴𝚂𝚂𝙾𝚁-𝙱𝙾𝚃 𝚟3.0.0"""
+✯ ᴄʀᴇᴀᴛᴇʀ : <a href=https://t.me/bloody_mikey>ᴍɪᴋᴇʏ sᴀᴍᴀ</a>
+✯ ʟɪʙʀᴀʀʏ : ᴘʏʀᴏɢʀᴀᴍ
+✯ ʟᴀɴɢᴜᴀɢᴇ: ᴘʏᴛʜᴏɴ 𝟹
+✯ ᴅᴀᴛᴀ ʙᴀsᴇ : ᴍᴏɴɢᴏ-ᴅʙ
+✯ ʙᴏᴛ sᴇʀᴠᴇʀ : ʜᴇʀᴏᴋᴜ
+✯ ʙᴜɪʟᴅ ᴠᴇʀsɪᴏɴ: ᴡᴇᴅɴᴇsᴅᴀʏ-ʙᴏᴛ ᴠ𝟹.𝟶.𝟶"""
 
-    SOURCE_TXT = """<b>NOTE:</b>
-- 𝚂𝙾𝚄𝚁𝙲𝙴 𝙲𝙾𝙳𝙴 𝙲𝙻𝙸𝙲𝙺 𝙷𝙴𝚁 👉 :<a href=https://github.com/MrMKN/PROFESSOR-BOT>𝐏𝐑𝐎𝐅𝐄𝐒𝐒𝐎𝐑-𝐁𝐎𝐓</a>
+    SOURCE_TXT = """<b> ᴩʜуꜱᴄᴏ ᴀssᴏᴄɪᴀᴛɪᴏɴ</b>
 
-<b>DEVS:</b>
-- 𝙳𝚎𝚟 1<a href=https://t.me/mr_MKN>𝙼𝚛.𝙼𝙺𝙽 𝚃𝙶</a>
-- 𝙳𝚎𝚟 2<a href=https://t.me/AboutAadhi>𝙰𝙰𝙳𝙷𝙸</a>"""
+-ᴩʜуꜱᴄᴏ ɪs ᴀɴ ᴀɴɪᴍᴇ ᴄᴏᴍᴍᴜɴɪᴛʏ ᴡʜɪᴄʜ ɪs ᴀ sᴇʀᴠɪᴄᴇ ᴘʀᴏᴠɪᴅɪɴɢ ɴᴇᴛᴡᴏʀᴋ ғᴏʀ ᴀɴɪᴍᴇ ʀᴇʟᴀᴛᴇᴅ sᴛᴜғғs ᴀᴄʀᴏss ᴅɪғғᴇʀᴇɴᴛ ᴘʟᴀᴛғᴏʀᴍs. ɪᴛ ɪs ᴀ ᴄᴏᴍᴍᴜɴɪᴛʏ ᴍᴀᴅᴇ ғᴏʀ ʀᴀɪsɪɴɢ ᴜᴘ ᴏғ ᴀɴɪᴍᴇ ᴄᴏɴᴛᴇɴᴛ ɪɴ ɪɴᴅɪᴀ.</a>
+
+<b>ᴡʜʏ ʏᴏᴜ sʜᴏᴜʟᴅ sᴜᴘᴘᴏʀᴛ ᴩʜуꜱᴄᴏ</b>
+
+-ᴡᴇ ɴᴇᴇᴅ sᴜᴘᴘᴏʀᴛ ᴏғ ᴛʜᴇ ᴀɴɪᴍᴇ ᴠɪᴇᴡᴇʀs ᴛᴏ ᴋᴇᴇᴘ ᴜs ᴍᴏᴛɪᴠᴀᴛᴇᴅ ᴛᴏ ɪɴᴄʀᴇᴀsᴇ ᴏᴜʀ sᴇʀᴠɪᴄᴇ ғᴏʀ ᴇᴠᴇʀʏᴏɴᴇ.  ʏᴏᴜʀ sᴜᴘᴘᴏʀᴛ ᴍᴀᴋᴇs ᴜs ᴘᴜsʜ ᴏᴜʀsᴇʟᴠᴇs ᴀ sᴛᴇᴘ ғᴜʀᴛʜᴇʀ. ᴡᴇ ᴋɴᴏᴡ ᴏᴜʀ ᴅʀᴇᴀᴍ ᴏғ ʙʀɪɴɢɪɴɢ ᴀɴɪᴍᴇ ɪɴ ɪɴᴅɪᴀ ɪs sɪʟʟʏ ʙᴜᴛ ᴡʜᴀᴛ's ᴛʜᴇ ᴘᴏɪɴᴛ ᴏғ ɪᴛ, ɪғ ᴡᴇ ᴄᴀɴ'ᴛ ʙᴇʟɪᴇᴠᴇ ɪɴ ᴏᴜʀsᴇʟᴠᴇs. ᴡᴇ ᴡᴏɴ'ᴛ ɢɪᴠᴇ ᴜᴘ ᴏɴ ᴏᴜʀ ᴅʀᴇᴀᴍ. ʀᴇsᴛ ɪs ᴜᴘᴏɴ ʏᴏᴜ ɢᴜʏs, ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ sᴜᴘᴘᴏʀᴛ ᴏʀ ɴᴏᴛ."""
 
     FILE_TXT = """➤ 𝐇𝐞𝐥𝐩: 𝐅𝐢𝐥𝐞 𝐒𝐭𝐨𝐫𝐞 𝐌𝐨𝐝𝐮𝐥𝐞../
 
@@ -28,9 +29,9 @@ class script(object):
 
 ⪼ 𝐄𝐱𝐚𝐦𝐩𝐥𝐞 ›
 
-<code>/batch https://t.me/mkn_bots_updates https://t.me/mkn_bots_updates</code>
+<code>/batch https://t.me/everytype_movies2 https://t.me/everytype_movies7</code>
 
-𝙲𝚁𝙴𝙳𝙸𝚃𝚂 ›› <a href=https://t.me/mkn_bots_updates><b>𝙼𝙺𝙽 𝙱𝙾𝚃𝚉</b></a>"""
+ ᴏᴡɴᴇʀ :- <a href=https://t.me/zeeroworld>ᴄʟɪᴄᴋ ʜᴇʀᴇ</a>**"""
     
     MANUELFILTER_TXT = """Help: <b>Filters</b>
 
@@ -76,7 +77,7 @@ class script(object):
 ›› /set_template - 𝚂𝙴𝚃 𝙲𝚄𝚂𝚃𝙾𝙼 𝙸𝙼𝙳𝙱 𝚃𝙴𝙼𝙿𝙻𝙰𝚃𝙴 𝙵𝙾𝚁 𝙰𝚄𝚃𝙾 𝙵𝙸𝙻𝚃𝙴𝚁. 
 ›› /get_template - 𝙶𝙴𝚃 𝙲𝚄𝚁𝚁𝙴𝙽𝚃 𝙸𝙼𝙳𝙱 𝚃𝙴𝙼𝙿𝙻𝙰𝚃𝙴 𝙾𝙵 𝙰𝚄𝚃𝙾 𝙵𝙸𝙻𝚃𝙴𝚁.
 
-𝙲𝚁𝙴𝙳𝙸𝚃𝚂 :- <a href=https://t.me/mr_MKN>Mr.MKN TG</a>**"""
+ᴏᴡɴᴇʀ :- <a href=https://t.me/zeeroworld>ᴄʟɪᴄᴋ ʜᴇʀᴇ</a>**"""
 
     CONNECTION_TXT = """Help: <b>Connections</b>
 
@@ -185,7 +186,7 @@ By @{} """
 
 ➪ 𝖩𝗎𝗌𝗍 𝗌𝖾𝗇𝖽 𝗆𝖾 𝖺 𝗂𝗆𝖺𝗀𝖾 𝗍𝗈 𝖾𝖽𝗂𝗍 ✨
 
-𝖬𝖺𝖽𝖾 𝖻𝗒 <a href=https://t.me/mr_MKN>Mr.MKN TG</a>"""
+ᴏᴡɴᴇʀ :- <a href=https://t.me/zeeroworld>ᴄʟɪᴄᴋ ʜᴇʀᴇ</a>**"""
 
     RESTRIC_TXT = """➤ 𝐇𝐞𝐥𝐩: Mᴜᴛᴇ 🚫
 
